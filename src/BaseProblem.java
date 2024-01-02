671a94696f54e544472cac5810fb5950bb073303
wlgks7460 <wlgks7460@naver.com>
2024-01-02 14:44:32 +0900

    01-02

--- src/BaseProblem.java
+++ src/BaseProblem.java
@@ -1,14 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class BaseProblem {
     public static void main(String[] args) {
         int n = 125;
         int answer = 0;
-        String temp = "";
-        while (n>0){
-            temp += Integer.toString(n%3);
+        List<Integer> temp = new ArrayList<>();
+        while(n!=0){
+            temp.add(n%3);
+            n/=3;
         }
-        int
-        for(int i=0; i<temp.length(); i++){
+        int a = 1;
+        for(int i =temp.size()-1 ; i>=0; i--){
+            answer += temp.get(i)*a;
+            a *= 3;
 
         }
+        System.out.println(answer);
     }
 }
